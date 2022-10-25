d6a5aa56c9e4d04fb71d9724f7db16b29a2e27fe
Kumar <aquaraga@gmail.com>
2022-10-25 23:08:15 +0530

    Using Springboot async
    https://www.baeldung.com/spring-mvc-async-vs-webflux

--- src/main/java/com/reactivedemo/events/EventController.java
+++ src/main/java/com/reactivedemo/events/EventController.java
@@ -3,10 +3,12 @@ package com.reactivedemo.events;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
 
 @RestController
 public class EventController {
@@ -15,22 +17,23 @@ public class EventController {
     EventService service;
 
     @GetMapping("/events")
-    public ResponseEntity<List<Event>> getAllEvents() {
+    @Async("asyncExecutor")
+    public CompletableFuture<ResponseEntity<List<Event>>> getAllEvents() {
         System.out.println("AllEvents request served by: "
                 + Thread.currentThread().getName());
 
-        List<Event> allEvents = service.getAllEvents();
+        return service.getAllEvents()
+                .thenApply(events -> new ResponseEntity<>(events, HttpStatus.OK));
 
-        return new ResponseEntity<>(allEvents, HttpStatus.OK);
     }
 
     @GetMapping("/upcoming-event")
-    public ResponseEntity<Event> getUpcomingEvent() {
+    @Async("asyncExecutor")
+    public CompletableFuture<ResponseEntity<Event>> getUpcomingEvent() {
         System.out.println("UpcomingEvent request served by: "
                 + Thread.currentThread().getName());
 
-        Event event = service.upcomingEvent();
-
-        return new ResponseEntity<>(event, HttpStatus.OK);
+        return service.upcomingEvent()
+                .thenApply(event -> new ResponseEntity<>(event, HttpStatus.OK));
     }
 }
--- src/main/java/com/reactivedemo/events/EventService.java
+++ src/main/java/com/reactivedemo/events/EventService.java
@@ -2,9 +2,11 @@ package com.reactivedemo.events;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
 
 @Service
 public class EventService {
@@ -12,13 +14,14 @@ public class EventService {
     @Autowired
     RestTemplateBuilder restTemplateBuilder;
 
-    public List<Event> getAllEvents() {
+    public CompletableFuture<List<Event>> getAllEvents() {
         restTemplateBuilder.build().getForObject("https://httpbin.org/delay/30", ResponseStub.class);
-        return List.of(new Event("1", "Geeknight-Bangalore", "Blockchain technologies"));
+        return CompletableFuture.completedFuture(List.of(new Event("1", "Geeknight-Bangalore", "Blockchain technologies")));
     }
 
-    public Event upcomingEvent() {
-        return new Event("1", "Reactive-workshop-Bangalore", "Reactive programming concepts");
+    @Async("asyncExecutor")
+    public CompletableFuture<Event> upcomingEvent() {
+        return CompletableFuture.completedFuture(new Event("1", "Reactive-workshop-Bangalore", "Reactive programming concepts"));
     }
 }
 
